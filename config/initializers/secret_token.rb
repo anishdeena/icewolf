814ee182052431fa541f38577225bd5d4f6fa3f7
anishdeena <anishdeena@gmail.com>
2013-03-31 02:11:55 +0530

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Icewolf::Application.config.secret_token = '1c5a68be36ef0fa8681d285fb7107106c4efa5d265499e647398c7b1daddd9ec1ad3656a78cc7524724240293bb198cf84f3d972b751c78b796d9544f1bc967b'

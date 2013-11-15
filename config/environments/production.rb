54f9a1a7ee7f369326ef9312e421da97e816023a
anishdeena <anishdeena@gmail.com>
2013-11-15 17:10:26 +0530

    asset aws s3 bucket specified

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,8 @@ Icewolf::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.action_controller.asset_host = "http://icewolf.s3.amazonaws.com"
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
